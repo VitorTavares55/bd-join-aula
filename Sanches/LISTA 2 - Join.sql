bfda162418a52ab9b8e74de9d9525f6ef54215b8
Rafael Sanches <81397822+Xanxesss@users.noreply.github.com>
2022-10-09 16:18:09 -0300

    Resolução completa da LISTA 2 - JOIN

--- Sanches/LISTA 2 - Join.sql
+++ Sanches/LISTA 2 - Join.sql
@@ -34,24 +34,71 @@ AND (cidade.nome = 'MARINGÁ' OR cidade.nome = 'PARANAVAÍ' OR cidade.nome = 'CI
 
 -- Nível 2
 -- 5. Informe o nome dos produtos e a respectiva unidade de medida da venda com o código 3.
+SELECT produto.nome, unidade_medida.nome
+FROM produto, unidade_medida, venda, item_venda
+WHERE unidade_medida.id_unidade_medida = produto.id_unidade_medida
+AND produto.id_produto = item_venda.id_produto
+AND venda.id_venda = item_venda.id_venda
+AND venda.id_venda = 3;
 
 -- 6. Liste os nomes dos funcionários que já venderam o produto 'REFRIGERANTE COCA-COLA GARRAFA PET 3 L'.
+SELECT funcionario.nome
+FROM funcionario, venda, item_venda, produto
+WHERE funcionario.id_funcionario = venda.id_funcionario
+AND venda.id_venda = item_venda.id_venda
+AND produto.id_produto = item_venda.id_produto
+AND produto.nome = "REFRIGERANTE COCA-COLA GARRAFA PET 3 L";
 
 -- 7. Liste os nomes dos fornecedores dos estados PARANÁ, MATO GROSSO e RIO GRANDE DO NORTE.
+-- Não há a tabela 'fornecedores' nessa Base de Dados.
 
-8. Liste os nomes dos produtos vendidos no dia XXX (pode escolher um dia existente).
+-- 8. Liste os nomes dos produtos vendidos no dia XXX (pode escolher um dia existente).
+SELECT produto.nome
+FROM produto, item_venda, venda
+WHERE produto.id_produto = item_venda.id_produto
+AND venda.id_venda = item_venda.id_venda
+AND venda.data = '1975-05-22 19:05:03';
 
-9. Feliz com um bom atendimento, um determinado cliente entra em contato com o gerente para enviar uma lembrança ao funcionário.
-O problema é que, além de não informar a sua identidade, ele não lembra do nome do funcionário. Sabe apenas que,na conversa, moraram 
-na mesma cidade e possuem o mesmo nome. Neste contexto, faça as consultas necesárias para identificar o funcionário.
+-- 9. Feliz com um bom atendimento, um determinado cliente entra em contato com o gerente para enviar uma lembrança ao funcionário.
+-- O problema é que, além de não informar a sua identidade, ele não lembra do nome do funcionário. Sabe apenas que, na conversa, moraram 
+-- na mesma cidade e possuem o mesmo nome. Neste contexto, faça as consultas necesárias para identificar o funcionário.
+SELECT funcionario.nome
+FROM funcionario, cliente, venda, cidade
+WHERE cliente.id_cliente = venda.id_cliente
+AND funcionario.id_funcionario = venda.id_funcionario
+AND cliente.nome = funcionario.nome
+AND cliente.id_cidade = funcionario.id_cidade;
 
-10. Um cliente entra em contato com o gerente solicitando um possível erro de troco. O cliente não lembra o nome do funcionário, 
-mas possui as seguintes informações:
- (1) nome completo do cliente: Sophia Lima
- (2) CPF do cliente: 321.109.923-95
- (3) cidade onde cliente nasceu: BAURU/SÃO PAULO
- (4) a cliente alega estar faltando 30 reais
-Neste contexto, faça as consultas necessárias para descobrir o possível nome do funcionário. Justifique!
+-- 10. Um cliente entra em contato com o gerente solicitando um possível erro de troco. O cliente não lembra o nome do funcionário, 
+-- mas possui as seguintes informações:
+--  (1) nome completo do cliente: Sophia Lima
+--  (2) CPF do cliente: 321.109.923-95
+--  (3) cidade onde cliente nasceu: BAURU/SÃO PAULO
+--  (4) a cliente alega estar faltando 30 reais
+-- Neste contexto, faça as consultas necessárias para descobrir o possível nome do funcionário. Justifique!
+SELECT funcionario.nome
+FROM funcionario, cliente, venda
+WHERE cliente.cpf_cnpj = "321.109.923-95"
+AND cliente.id_cliente = venda.id_cliente
+AND funcionario.id_funcionario = venda.id_funcionario;
 
-11. De acordo com o projeto do SEU COLEGA, elabore uma consulta RELEVANTE que ajude de alguma forma a empresa. 
-A consulta deve envolver mais de 4 tabelas.
+-- 11. De acordo com o projeto do SEU COLEGA, elabore uma consulta RELEVANTE que ajude de alguma forma a empresa. 
+-- A consulta deve envolver mais de 4 tabelas.
+SELECT leitor.nome, categoria.classificacao, COUNT(*) AS qntd_categoria
+FROM categoria, livro, retirada_devolucao, leitor
+WHERE leitor.id_leitor = retirada_devolucao.id_leitor
+AND livro.id_livro = retirada_devolucao.id_livro
+AND categoria.id_categoria = livro.id_categoria
+AND retirada_devolucao.data_retirada BETWEEN "2022-08-01" AND "2022-08-31"
+GROUP BY leitor.nome, categoria.classificacao;
+
+-- A Consulta foi feita em meu próprio projeto, BLIBLIOTECA SNEK.
+-- O objetivo desse SELECT era consultar a quantia de livros que cada cliente retirou de cada classificação no mês de agosto.
+/*
+O resultado é exibido de forma:
+Cliente_1 retirou v livros de Categoria_1.
+Cliente_1 retirou w livros de Categoria_2.
+Cliente_2 retirou x livros de Categoria_1.
+Cliente_2 retirou y livros de Categoria_2.
+Cliente_2 retirou z livros de Categoria_3.
+*/
